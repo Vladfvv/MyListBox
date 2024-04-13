cb706747521a740b724590e78f4e2150f21e2c09
Vladislav Filimontsev <vladfilimontsev@yandex.ru>
2024-04-13 23:05:00 +0300

    Add project files.

---
+++ MyListBox/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Calculate
+        {
+            public double xStart { get; set; }
+            public double xStop { get; set; }
+            public double step { get; set; }
+            public double n { get; set; }  
+
+        }
+        
+        Calculate calculate;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculate = new Calculate();
+            grid.DataContext = calculate;
+
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
