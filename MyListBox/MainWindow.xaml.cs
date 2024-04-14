0bb3f6e01366cddb00de9adb70f987274f30407f
Vladislav Filimontsev <vladfilimontsev@yandex.ru>
2024-04-15 00:07:56 +0300

    Without y(x) - function

--- MyListBox/MainWindow.xaml.cs
+++ MyListBox/MainWindow.xaml.cs
@@ -24,20 +24,22 @@ namespace MyListBox
             public double xStop { get; set; }
             public int step { get; set; }
             private int n;
-        
 
-        public int N
+
+            public int N
             {
                 get { return n; }
                 set
                 {
-                    if (value < 5)  throw new ArgumentException("Value must be greater than 5"); 
+                    //if (value < 5)  throw new ArgumentException("Value must be greater than 5"); 
                     n = value;
                 }
             }
 
         }
         Values values;
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -47,5 +49,22 @@ namespace MyListBox
             lResults.DataContext = results;
         }
 
+        private void calcButton_Click(object sender, RoutedEventArgs e)
+        {
+            double summa = 0;
+            values.xStart = double.Parse(xStart.Text);
+            values.xStop = double.Parse(xStop.Text);
+            values.step = int.Parse(step.Text);
+            values.N = int.Parse(n.Text);
+            double yXStart = -1 / 2 * Math.Log(1 - 2 * values.xStart * Math.Cos(Math.PI / 3) + Math.Pow(values.xStart, 2));
+
+            for (var k = 1; k < values.N; k++)
+            {
+                summa += (Math.Pow(values.xStart, k) * Math.Cos(k * Math.PI / 3)) / 3;
+                results.Add("Summa: " + summa);
+            }
+
+            summa = 0;
+        }
     }
 }
